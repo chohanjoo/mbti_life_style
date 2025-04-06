386c2d9ecbda2801283d74fc33a295ad87d35172
hanjoo96.cho <hanjoo96.cho@navercorp.com>
2025-04-06 17:53:26 +0900

    Set up GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mbti_life_style/'
+  base: 'https://chohanjoo.github.io/mbti_life_style/'
 })
